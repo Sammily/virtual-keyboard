5f4d266347c4278005ed93884e5ab0e3f115d589
Dasha <sammilywow@gmail.com>
2022-05-05 16:29:50 +0300

    feat: add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,46 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
     clean: true,
   },
-  mode: 'development',
-  plugins: [new HtmlWebpackPlugin({
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
       //template: './src/index.html'
-  })],
+    }),
+  ],
   module: {
-      rules: [
-        {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
           },
-        {
-            test: /\.s[ac]ss$/i,
-            use: ["style-loader", "css-loader", "sass-loader"],
-          },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
-            type: 'asset/resource',
-          },
-      ]
-    },
-    resolveLoader: {
-        extensions: ['.js']
-      }
+        },
+      },
+    ],
+  },
+  resolveLoader: {
+    extensions: [".js"],
+  },
 };
